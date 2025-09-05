43ba0c5757d70f66c33352341c3e43a13d2dc41b
Caleb Pace <pacecaleb2004+Git@gmail.com>
2025-09-05 17:38:36 +1200

    feat(Client): use binary to store and transport contacts

---
+++ ShadowWire.Desktop.Client/Users/ContactBinarySerializer.cs
@@ -0,0 +1,47 @@
+﻿namespace ShadowWire.Desktop.Client.Users
+{
+    public static class ContactBinarySerializer
+    {
+        public static byte[] Serialize(Contact contact)
+        {
+            var memStream = new MemoryStream();
+
+            using (var binWrite = new BinaryWriter(memStream))
+            {
+                binWrite.Write(contact.Nickname);
+
+                binWrite.Write(contact.Fingerprint.Length);
+                binWrite.Write(contact.Fingerprint);
+
+                binWrite.Write(contact.PublicKeyDer.Length);
+                binWrite.Write(contact.PublicKeyDer);
+            }
+
+            return memStream.ToArray();
+        }
+
+        // TODO: Implement error handling
+        public static Contact? Deserialize(byte[] serializedContact)
+        {
+            Contact? contact = null;
+
+            using (var memStream = new MemoryStream(serializedContact))
+            using (var binReader = new BinaryReader(memStream))
+            {
+                int length;
+
+                string nickname = binReader.ReadString();
+
+                length = binReader.ReadInt32();
+                byte[] fingerprint = binReader.ReadBytes(length);
+
+                length = binReader.ReadInt32();
+                byte[] publicKeyDer = binReader.ReadBytes(length);
+
+                contact = new Contact(nickname, fingerprint, publicKeyDer);
+            }
+
+            return contact;
+        }
+    }
+}
--- TimeLog.md
+++ TimeLog.md
@@ -14,3 +14,8 @@ Client:
 - Connection: 32m
 - Cryptography: 32m
     - Research: 12m
+- Contacts: 35m 1m
+    - Planning: 5m
+    - Persistence: 2m
+        - Bson issue: 45m 4m
+    - Contact Packaging: 20m
