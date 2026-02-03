d795c0451c887dacc4e4ae3cc4468a0dd2d21980
Caleb Pace <pacecaleb2004+Git@gmail.com>
2026-02-04 11:49:52 +1300

    refactor(Shared/Protocols/Identification): use ContactBinaryCodec for encoding to remove duplicate code

--- ShadowWire.Shared/Protocols/IdentificationProtocol/IdentificationProtocol.cs
+++ ShadowWire.Shared/Protocols/IdentificationProtocol/IdentificationProtocol.cs
@@ -1,38 +1,21 @@
-﻿using ShadowWire.Shared.BinaryEncoding;
-using ShadowWire.Shared.Users;
+﻿using ShadowWire.Shared.Users;
 
 namespace ShadowWire.Shared.Protocols.IdentificationProtocol;
 
 public class IdentificationProtocol : IProtocol<Contact>
 {
-    public ProtocolId Id => ProtocolId.Identification;
+    public ProtocolId Protocol => ProtocolId.Identification;
 
 
     public Contact Decode(ReadOnlySpan<byte> packetBinary)
     {
-        var reader = new SpanReader(packetBinary);
-
-        var nickname     = reader.ReadString();
-        var fingerprint  = reader.ReadBytes().ToArray();
-        var publicKeyDer = reader.ReadBytes().ToArray();
-
-        return new Contact(nickname, fingerprint, publicKeyDer);
+        ContactBinaryCodec.TryDecode(packetBinary, out Contact contact);
+        return contact;
     }
 
     public ReadOnlySpan<byte> Encode(Contact packetStruct)
     {
-        var length = packetStruct.Nickname.Length
-                   + packetStruct.Fingerprint.Length
-                   + packetStruct.PublicKeyDer.Length
-                   + (3 * sizeof(Int32));
-
-        var buffer = new byte[length];
-        var writer = new SpanWriter(new Span<byte>(buffer));
-
-        writer.WriteString(packetStruct.Nickname);
-        writer.WriteBytes(packetStruct.Fingerprint);
-        writer.WriteBytes(packetStruct.PublicKeyDer);
-
-        return buffer;
+        var innerPacket = ContactBinaryCodec.Encode(packetStruct);
+        return ProtocolHeader.PrependProtocolHeader(innerPacket, Protocol);
     }
 }
