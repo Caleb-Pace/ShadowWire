b51916207615374d863bf7d54e0cd30d43b5c8d3
Caleb Pace <pacecaleb2004+Git@gmail.com>
2026-02-06 13:17:37 +1300

    feat(Server/Network/SessionHandler): add message handler selection logic

--- ShadowWire.Server/Network/SessionHandler.cs
+++ ShadowWire.Server/Network/SessionHandler.cs
@@ -1,9 +1,10 @@
-﻿using ShadowWire.Shared.Users;
+﻿using ShadowWire.Server.Handlers;
+using ShadowWire.Shared.Users;
 using System.Net.WebSockets;
 
 namespace ShadowWire.Server.Network;
 
-internal class SessionHandler(WebSocket webSocket, Func<byte[], byte[], Task> routeMessageAsync, ContactManager userRegistry)
+public class SessionHandler(WebSocket webSocket, Func<byte[], byte[], Task> routeMessageAsync, ContactManager userRegistry)
 {
     public Guid Id { get; private set; } = Guid.NewGuid(); // Create ID for new conneciton
     public WebSocket WebSocket { get; private set; } = webSocket;
@@ -11,8 +12,18 @@ internal class SessionHandler(WebSocket webSocket, Func<byte[], byte[], Task> ro
     private readonly Func<byte[], byte[], Task> _routeMessageAsync = routeMessageAsync; // (Fingerprint, Binary)
     private readonly ContactManager _userRegistry = userRegistry;
 
+    private static Dictionary<byte, IMessageHandler> MessageHandlers => new();
 
-    public async Task ReceiveMessage(byte[] buffer)
+
+    public async Task ReceiveMessageAsync(byte[] buffer)
     {
+        var messageKind = buffer[0];
+        if (MessageHandlers.TryGetValue(messageKind, out IMessageHandler messageHandler))
+        {
+            await messageHandler.HandleAsync(this, buffer);
+            return;
+        }
+
+        // TODO: Implement logging - Unknown message kind
     }
 }
