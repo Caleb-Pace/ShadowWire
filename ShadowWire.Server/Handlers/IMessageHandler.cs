128bd593eecfb428293a5e6aca896394f69dbdbd
Caleb Pace <pacecaleb2004+Git@gmail.com>
2026-02-18 23:53:44 +1300

    cleanup(Server/Handlers/): remove redundant class IMessageHandler

--- ShadowWire.Server/Handlers/IMessageHandler.cs
+++
@@ -1,8 +0,0 @@
-﻿using ShadowWire.Server.Network;
-
-namespace ShadowWire.Server.Handlers;
-
-public interface IMessageHandler
-{
-    Task HandleAsync(ClientSession session, byte[] messageInBytes);
-}
