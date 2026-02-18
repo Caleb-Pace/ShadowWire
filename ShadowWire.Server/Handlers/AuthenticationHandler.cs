f4ca60b9f2d85c1f86cf31bf374c9be1b3010a0d
Caleb Pace <pacecaleb2004+Git@gmail.com>
2026-02-19 00:00:26 +1300

    refactor(Server/Handlers/Authentication): use new message handler system

--- ShadowWire.Server/Handlers/AuthenticationHandler.cs
+++ ShadowWire.Server/Handlers/AuthenticationHandler.cs
@@ -1,6 +1,6 @@
 ﻿using ShadowWire.Server.Network;
 using ShadowWire.Shared.Protocol;
-using ShadowWire.Shared.Users;
+using ShadowWire.Shared.Protocol.Packets;
 
 namespace ShadowWire.Server.Handlers;
 
@@ -10,45 +10,15 @@ namespace ShadowWire.Server.Handlers;
 /// <remarks>
 /// Follows the singleton design pattern and implements <see cref="IMessageHandler"/>.
 /// </remarks>
-public class AuthenticationHandler : IMessageHandler
+public class AuthenticationHandler : IMessageHandler<ClientSession, AuthenticationRequest>
 {
     public static AuthenticationHandler Instance => new();
 
 
-    private void AttachClientIdentityToSession(ClientSession session, ReadOnlySpan<byte> contactBinary)
+    public async Task<IEncodable> HandleAsync(ClientSession context, AuthenticationRequest request)
     {
-        if (ContactBinaryCodec.TryDecode(contactBinary, out var contact))
-            session.AssignClientIdentity(contact);
-    }
+        context.AssignClientIdentity(request.contact);
 
-    /// <summary>
-    /// Handles incoming identification messages.
-    /// </summary>
-    /// <param name="session">The client session.</param>
-    /// <param name="messageInBytes">The message bytes, including the kind byte.</param>
-    /// <exception cref="ArgumentNullException">
-    /// Thrown if <paramref name="session"/> is null.
-    /// </exception>
-    /// <exception cref="ArgumentOutOfRangeException">
-    /// Thrown if <paramref name="messageInBytes"/> is too short to be a valid identification message.
-    /// </exception>
-    public async Task HandleAsync(ClientSession session, byte[] messageInBytes)
-    {
-        // Validation
-        ArgumentNullException.ThrowIfNull(session);
-        ArgumentOutOfRangeException.ThrowIfLessThan(messageInBytes.Length, 2, nameof(messageInBytes));
-
-        // Handle
-        byte kind = messageInBytes[0];
-        ReadOnlySpan<byte> content = messageInBytes.AsSpan(1); // Skip message kind
-        switch (kind)
-        {
-            case (byte)MessageKind.AuthenticationRequest:
-                AttachClientIdentityToSession(session, content); 
-                break;
-            default:
-                // TODO: Implement logging - Warning: Unsupported message kind
-                break;
-        }
+        return new AuthenticationSuccess();
     }
 }
