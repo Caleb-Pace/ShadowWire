1b66807904716731cd05d1be1a68c78155b02af7
Caleb Pace <pacecaleb2004+Git@gmail.com>
2026-02-24 13:27:13 +1300

    feat(Client/Services/): implement basic authentication service

---
+++ ShadowWire.Desktop.Client/Services/AuthenticationService.cs
@@ -0,0 +1,16 @@
+﻿using ShadowWire.Desktop.Client.Network;
+using ShadowWire.Shared.Protocol.Messages;
+using ShadowWire.Shared.Users;
+
+namespace ShadowWire.Desktop.Client.Services;
+
+public class AuthenticationService
+{
+    public static async Task SendAsync(Connection connection, Contact contact)
+    {
+        var request = new AuthenticationRequest(contact);
+        var requestBinary = request.Encode();
+
+        await connection.SendAsync(requestBinary);
+    }
+}
