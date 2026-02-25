c7bffe13556153d8cfcf24512631e8ca3c9bb130
Caleb Pace <pacecaleb2004+Git@gmail.com>
2026-02-25 19:07:14 +1300

    feat(Shared/Protocol/Messages/): create LookupResponse using ContactMessageBase

---
+++ ShadowWire.Shared/Protocol/Messages/LookupResponse.cs
@@ -0,0 +1,12 @@
+﻿using ShadowWire.Shared.Users;
+
+namespace ShadowWire.Shared.Protocol.Messages;
+
+public sealed class LookupResponse : ContactMessageBase
+{
+    protected override MessageKind Kind => MessageKind.LookupResponse;
+
+
+    public LookupResponse(Contact contact) : base(contact) { }
+    public LookupResponse(ReadOnlySpan<byte> messageBytes) : base(messageBytes) { }
+}
