044c6558fe55f0a8229c571916ec26cf9c789169
Caleb Pace <pacecaleb2004+Git@gmail.com>
2026-02-25 19:06:47 +1300

    refactor(Shared/Protocol/Messages/AuthenticationRequest): use ContactMessageBase

--- ShadowWire.Shared/Protocol/Messages/AuthenticationRequest.cs
+++ ShadowWire.Shared/Protocol/Messages/AuthenticationRequest.cs
@@ -1,32 +1,12 @@
-﻿using ShadowWire.Shared.BinaryEncoding;
-using ShadowWire.Shared.Users;
+﻿using ShadowWire.Shared.Users;
 
 namespace ShadowWire.Shared.Protocol.Messages;
 
-public readonly struct AuthenticationRequest : IEncodable
+public sealed class AuthenticationRequest : ContactMessageBase
 {
-    private const MessageKind MESSAGE_KIND = MessageKind.AuthenticationRequest;
-
-    public readonly Contact contact;
+    protected override MessageKind Kind => MessageKind.LookupResponse;
 
 
-    public AuthenticationRequest(Contact contact)
-        => this.contact = contact;
-
-    /// <exception cref="ArgumentException">Thrown if the byte span cannot be decoded.</exception>
-    public AuthenticationRequest(ReadOnlySpan<byte> messageBytes)
-    {
-        const int MINIMUM_LENGTH = 1 + (3 * sizeof(Int32));
-
-        ArgumentOutOfRangeException.ThrowIfLessThan(messageBytes.Length, MINIMUM_LENGTH, nameof(messageBytes));
-
-        ReadOnlySpan<byte> payloadBytes = messageBytes[1..]; // Skip message kind
-        if (!ContactBinaryCodec.TryDecode(payloadBytes, out var contact))
-            throw new ArgumentException("Invalid contact!");
-
-        this.contact = contact;
-    }
-
-    public byte[] Encode()
-        => ByteArrayUtils.PrependMessageKind(MESSAGE_KIND, ContactBinaryCodec.Encode(contact));
+    public AuthenticationRequest(Contact contact) : base(contact) { }
+    public AuthenticationRequest(ReadOnlySpan<byte> messageBytes) : base(messageBytes) { }
 }
