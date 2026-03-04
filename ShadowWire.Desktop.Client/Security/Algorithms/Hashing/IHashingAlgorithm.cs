30b6dabc181bed3f5221a4592e05149e0fdb0e7a
Caleb Pace <pacecaleb2004+Git@gmail.com>
2026-03-04 18:28:14 +1300

    refactor(Client/Security/Algorithms/Hashing/Interface): rename methods and re-order HMAC arguments to follow common conventinos

--- ShadowWire.Desktop.Client/Security/Algorithms/Hashing/IHashingAlgorithm.cs
+++ ShadowWire.Desktop.Client/Security/Algorithms/Hashing/IHashingAlgorithm.cs
@@ -6,18 +6,18 @@
 public interface IHashingAlgorithm
 {
     /// <summary>
-    /// Hashes the specified <paramref name="data"/>.
+    /// Computes the Hash for the specified <paramref name="data"/>.
     /// </summary>
     /// <param name="data">The data to hash.</param>
     /// <returns>A fixed-length digest of the <paramref name="data"/>.</returns>
-    ReadOnlyMemory<byte> Hash(ReadOnlySpan<byte> data);
+    ReadOnlyMemory<byte> ComputeHash(ReadOnlySpan<byte> data);
 
 
     /// <summary>
-    /// Generates a Hash-based Message Authentication Code (HMAC) for the specified <paramref name="data"/>.
+    /// Computes a Hash-based Message Authentication Code (HMAC) for the specified <paramref name="data"/>.
     /// </summary>
-    /// <param name="data">The data to authenticate.</param>
     /// <param name="key">The secret key used for the message authentication code.</param>
-    /// <returns>The generated HMAC digest.</returns>
-    ReadOnlyMemory<byte> GenerateHMAC(ReadOnlySpan<byte> data, ReadOnlySpan<byte> key);
+    /// <param name="data">The data to authenticate.</param>
+    /// <returns>The computed HMAC digest.</returns>
+    ReadOnlyMemory<byte> ComputeHMAC(ReadOnlySpan<byte> key, ReadOnlySpan<byte> data);
 }
